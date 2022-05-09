ebce0432c42273138b7ebef5aa44c9c6f0e344a3
gnehs <jayuiop@gmail.com>
2022-05-09 23:01:40 +0800

    added logo video

--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,11 @@
     <v-row justify="center" align="center">
         <v-col cols="10" sm="8" md="6" lg="4">
             <div class="index-container">
-                <img src="logo.png" width="256" />
+                <video src="logo.mp4" width="256" height="256" autoplay loop muted></video>
                 <h1>浮水印寶寶</h1>
                 <p>協助您為重要證件圖片加上浮水印，防止遭他人盜用。</p>
                 <nuxt-link class="wr-btn" to="/generator">開始使用</nuxt-link>
-                <nuxt-link class="wr-btn non-bg" to="/about"
-                    >了解更多</nuxt-link
-                >
+                <nuxt-link class="wr-btn non-bg" to="/about">了解更多</nuxt-link>
             </div>
         </v-col>
     </v-row>
